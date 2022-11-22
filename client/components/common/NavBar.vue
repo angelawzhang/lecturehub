186d2b248f94ddc8dcd72492928962b6ae0a28c8
Allen Ding <aqding@mit.edu>
2022-11-22 15:10:53 -0500

    adding user frontend functionality

--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -4,78 +4,121 @@
 
 <template>
   <nav>
-    <div class="left">
-      <img src="../../public/logo.svg">
-      <h1 class="title">
-        LectureHub
-      </h1>
-    </div>
-    <div class="right">
-      <router-link to="/">
-        Home
-      </router-link>
-      <router-link
-        v-if="$store.state.username"
-        to="/account"
-      >
-        Account
-      </router-link>
-      <router-link
-        v-else
-        to="/login"
-      >
+    <div class="navContainer">
+      <h1 class="title">LectureHub</h1>
+      <div v-if="$store.state.username" class="routerContainer">
+        <router-link to="/"> Home </router-link>
+        <router-link to="/explore"> Explore </router-link>
+        <router-link to="/account"> Profile </router-link>
+        <!-- <router-link v-else to="/login"> Login </router-link> -->
+      </div>
+      <!-- <section class="alerts">
+        <article
+          v-for="(status, alert, index) in $store.state.alerts"
+          :key="index"
+          :class="status"
+        >
+          <p>{{ alert }}</p>
+        </article>
+      </section> -->
+      <router-link v-if="!$store.state.username" to="/login">
         Login
       </router-link>
+
+      <a v-else v-on:click="logout" href="">Logout</a>
     </div>
-    <section class="alerts">
-      <article
-        v-for="(status, alert, index) in $store.state.alerts"
-        :key="index"
-        :class="status"
-      >
-        <p>{{ alert }}</p>
-      </article>
-    </section>
   </nav>
 </template>
 
+<script>
+export default {
+  methods: {
+    async logout() {
+      const url = "/api/users/session";
+      const options = {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        credentials: "same-origin", // Sends express-session credentials with request
+      };
+
+      const callback = () => {
+        this.$router.push({ name: "Not Found" }); // Goes to Home page after signing out
+        this.$store.commit("alert", {
+          message: "You are now signed out!",
+          status: "success",
+        });
+      };
+
+      try {
+        const r = await fetch(url, options);
+        if (!r.ok) {
+          // If response is not okay, we throw an error and enter the catch block
+          const res = await r.json();
+          throw new Error(res.error);
+        }
+
+        this.$store.commit("setUsername", null);
+
+        callback();
+      } catch (e) {
+        this.$set(this.alerts, e, "error");
+        setTimeout(() => this.$delete(this.alerts, e), 3000);
+      }
+    },
+  },
+};
+</script>
 <style scoped>
 nav {
-    padding: 1vw 2vw;
-    background-color: #ccc;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: relative;
+  padding: 1vw 2vw;
+  background-color: #ccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
 }
 
 .title {
-    font-size: 32px;
-    margin: 0 5px;
+  font-size: 32px;
+  margin: 0 5px;
 }
 
 img {
-    height: 32px;
+  height: 32px;
 }
 
+.navContainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  text-align: center;
+}
+
+.routerContainer {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+}
 .left {
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .right {
-    font-size: 20px;
-    display: grid;
-    gap: 16px;
-    grid-auto-flow: column;
-    align-items: center;
+  font-size: 20px;
+  display: grid;
+  gap: 16px;
+  grid-auto-flow: column;
+  align-items: center;
 }
 
 .right a {
-    margin-left: 5px;
+  margin-left: 5px;
 }
 
 .alerts {
-    width: 25%;
+  width: 25%;
 }
 </style>
