e6871152c0efede5c1bf04cb93fcc761cdf18e69
Allen Ding <aqding@mit.edu>
2022-11-28 11:53:54 -0500

    adding backend for posts

---
+++ server/post/collection.ts
@@ -0,0 +1,79 @@
+import type { HydratedDocument, Types } from "mongoose";
+import type { Post } from "./model";
+import PostModel from "./model";
+
+class PostCollection {
+  /**
+   * Add a Post to the collection
+   *
+   * @param {string} authorId - The id of the author
+   * @param {string} lectureId - The id of the parent lecture
+   * @param {string | null} parentId - The id of the parent post,
+   * null if there is no parent post
+   * @param {string} content - The content of the post
+   * @returns {Promise<HydratedDocument<Post>>} - The created Post
+   */
+  static async addOne(
+    authorId: Types.ObjectId | string,
+    lectureId: Types.ObjectId | string,
+    parentId: Types.ObjectId | string | null = null,
+    content: string
+  ): Promise<HydratedDocument<Post>> {
+    const date = new Date();
+    const post = new PostModel({
+      authorId: authorId,
+      lectureId: lectureId,
+      parentId: parentId,
+      content: content,
+      dateCreated: date,
+    });
+
+    await post.save();
+    return post.populate("authorId");
+  }
+
+  /**
+   *
+   * Find all posts with a given parent lecture with lectureId.
+   *
+   * @param {string} lectureId - The ID of the parent lecture
+   * @returns {Promise<Array<HydratedDocument<Post>>>} - An array of matching posts
+   */
+  static async findAllByLecture(
+    lectureId: Types.ObjectId | string
+  ): Promise<Array<HydratedDocument<Post>>> {
+    return PostModel.find({ lectureId: lectureId })
+      .sort("-date")
+      .populate("authorId");
+  }
+
+  /**
+   *
+   * Updates a post in the collection
+   *
+   * @param {string} postId - The ID of the post to be updated
+   * @param {string} content - The new content of the post
+   * @returns {Promise<HydratedDocument<Post>>} - The updated post
+   */
+  static async updateOne(
+    postId: Types.ObjectId | string,
+    content: string
+  ): Promise<HydratedDocument<Post>> {
+    const post = await PostModel.findById(postId);
+    post.content = content;
+    await post.save();
+    return post.populate("authorId");
+  }
+
+  /**
+   *
+   * Delete a post from the collection.
+   *
+   * @param {string} postId - The ID of the post to be deleted
+   */
+  static async deleteOne(postId: Types.ObjectId | string): Promise<void> {
+    await PostModel.deleteOne({ _id: postId });
+  }
+}
+
+export default PostCollection;
---
+++ server/post/middleware.ts
@@ -0,0 +1,150 @@
+import type { Request, Response, NextFunction } from "express";
+import { isValidObjectId } from "mongoose";
+import PostCollection from "./collection";
+import PostModel from "./model";
+
+/**
+ * Checks if the lecture ID is a valid lecture
+ */
+const isValidLectureId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validLecture = req.query.lectureId
+    ? isValidObjectId(req.query.lectureId)
+    : isValidObjectId(req.body.lectureId);
+  const lectureId = req.query.lectureId
+    ? req.query.lectureId
+    : req.body.lectureId;
+  console.log(validLecture, lectureId);
+  // TODO: implement when LectureModel is added.
+  // const post = validLecture ? await LectureModel.findById(lectureId) : "";
+  // if (!post) {
+  //   res.status(404).json({
+  //     error: "Lecture ID is not valid",
+  //   });
+  //   return;
+  // }
+  next();
+};
+
+/**
+ * Checks if the post ID is a valid post
+ */
+const isValidPostId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validPost = req.body.postId;
+  const post = validPost ? await PostModel.findById(validPost) : "";
+  if (!post) {
+    res.status(404).json({
+      error: "Post ID is not valid",
+    });
+    return;
+  }
+  next();
+};
+
+/**
+ * Checks if the parent post ID is a valid post
+ */
+const isValidParentPostId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // no post ID to check
+  if (req.body.parentId === null || req.body.parentId === undefined) {
+    next();
+    return;
+  }
+  const validPost = isValidObjectId(req.body.parentId);
+  const post = validPost ? await PostModel.findById(req.body.parentId) : "";
+  if (!post) {
+    res.status(404).json({
+      error: "The post ID is not valid",
+    });
+    return;
+  }
+
+  next();
+};
+
+/**
+ * Checks if the parent post is actually a parent post, and not
+ * a child post
+ */
+const isValidParentPost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.body.parentId === null || req.body.parentId === undefined) {
+    next();
+    return;
+  }
+  const post = await PostModel.findById(req.body.parentId);
+  console.log("POST", post);
+  if (post.parentId !== null) {
+    res.status(405).json({
+      error: "Cannot create a child post on a non parent post",
+    });
+    return;
+  }
+  next();
+};
+
+/**
+ * Checks if the content of the post is not empty
+ * and not a series of whitespace
+ */
+const isValidPostContent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.body.content) {
+    res.status(400).json({
+      error: "No content provided",
+    });
+    return;
+  } else if (!req.body.content.trim()) {
+    res.status(400).json({
+      error: "Content cannot be empty",
+    });
+    return;
+  }
+
+  next();
+};
+
+/**
+ * Checks if the logged in user is the same as the
+ * original post author
+ */
+const isValidPostModifier = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const post = await PostModel.findById(req.body.postId);
+  if ((req.session.userId as string) !== post.authorId.toString()) {
+    res.status(403).json({
+      error: "Invalid post modifier",
+    });
+    return;
+  }
+  next();
+};
+
+export {
+  isValidLectureId,
+  isValidPostId,
+  isValidParentPostId,
+  isValidParentPost,
+  isValidPostContent,
+  isValidPostModifier,
+};
