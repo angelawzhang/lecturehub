54587ef1aa18475bdb3461979f3b535aea983fd4
Benjamin Gao <bgao01@mit.edu>
2022-12-04 21:57:02 -0500

    remove unneeded code

--- client/components/Account/AccountPage.vue
+++ client/components/Account/AccountPage.vue
@@ -18,8 +18,6 @@ import ChangeUsernameForm from "@/components/Account/ChangeUsernameForm.vue";
 import ChangePasswordForm from "@/components/Account/ChangePasswordForm.vue";
 import DeleteAccountForm from "@/components/Account/DeleteAccountForm.vue";
 import LogoutForm from "@/components/Account/LogoutForm.vue";
-import PostSection from "@/components/Posts/PostSection.vue";
-import AnnotationSection from "@/components/Annotations/AnnotationSection.vue"
 
 export default {
   name: "AccountPage",
@@ -28,8 +26,6 @@ export default {
     ChangePasswordForm,
     DeleteAccountForm,
     LogoutForm,
-    PostSection,
-    AnnotationSection
   },
 };
 </script>
